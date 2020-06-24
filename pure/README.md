a859cd086a1a0f43932de244facfbe49e981d57b
Alex Pshenychnyi <afw@afw.net.ua>
2020-06-24 20:25:24 +0200

    pure js boilerplate first commit

---
+++ pure/README.md
@@ -0,0 +1,8 @@
+Require sass or node-sass to be installed
+```shell
+npm install -g sass
+```
+or
+```shell
+npm install --save-dev node-sass
+```
---
+++ pure/webpack.config.js
@@ -0,0 +1,42 @@
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: "New project"
+    }),
+    new MiniCssExtractPlugin()],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/i,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'sass-loader',
+            options: {
+              implementation: require('sass')
+            }
+          }
+        ]
+      }
+    ]
+  },
+  devServer: {
+    contentBase: path.join(__dirname, 'dist'),    
+    compress: false,
+    port: 9000
+  },
+}
